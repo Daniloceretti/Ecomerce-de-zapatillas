c3b2773d01efea12ca8b0be108d80661c5001d31
Danilo ceretti <cerettidanilo@hotmail.com>
2022-07-07 11:46:04 -0300

    listo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Header } from "./componentes/Header";
+import "boxicons";
+import { BrowserRouter as Route } from "react-router-dom";
+import { Paginas } from "./componentes/Paginas";
+import { DataProvider } from "./context/Dataprovider";
+import {Carrito} from "./componentes/carrito"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DataProvider>
+      <div className="App">
+        <Route>
+          <Header />
+          <Carrito/>
+          <Paginas />
+        </Route>
+      </div>
+    </DataProvider>
   );
 }
 
---
+++ src/componentes/Productos/index.js
@@ -0,0 +1,27 @@
+import React, { useContext } from "react";
+import { DataContext } from "../../context/Dataprovider";
+import { ProductosItems } from "./ProductosItems";
+
+export const ProductosLista = () => {
+  const value = useContext(DataContext);
+  const [productos] = value.productos;
+
+  return (
+    <>
+      <h1 className="title">PRODUCTOS</h1>
+      <div className="productos">
+        {productos.map((producto) => (
+          <ProductosItems
+            key={producto.id}
+            id={producto.id}
+            title={producto.title}
+            price={producto.price}
+            image={producto.image}
+            category={producto.category}
+            cantidad={producto.cantidad}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
